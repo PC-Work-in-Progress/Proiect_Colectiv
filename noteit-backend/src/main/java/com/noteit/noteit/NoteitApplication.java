995e11a0d0888a11a0768fdcff039c0d13a557da
Jeler Cristiana-Ioana <jcir2532@scs.ubbcluj.ro>
2020-11-12 13:52:10 +0200

    Setup liquibase and database

--- noteit-backend/src/main/java/com/noteit/noteit/NoteitApplication.java
+++ noteit-backend/src/main/java/com/noteit/noteit/NoteitApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NoteitApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NoteitApplication.class, args);
 	}
-
 }
--- noteit-backend/src/main/java/com/noteit/noteit/controller/TestController.java
+++ noteit-backend/src/main/java/com/noteit/noteit/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.noteit.noteit.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
 @CrossOrigin
+@AllArgsConstructor
 public class TestController {
-
     @GetMapping("/da")
     public ResponseEntity<?> justATest(){
             return ResponseEntity.ok().body("Merge");
