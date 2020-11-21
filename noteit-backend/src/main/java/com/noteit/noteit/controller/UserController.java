fd6ca380161bdfce8c07dac1f174a6f5453c3b71
andreea <iloaiaandreea@gmail.com>
2020-11-21 19:19:25 +0200

    endpoint user's details token from header

--- noteit-backend/src/main/java/com/noteit/noteit/controller/UserController.java
+++ noteit-backend/src/main/java/com/noteit/noteit/controller/UserController.java
@@ -6,6 +6,9 @@ import org.hibernate.service.spi.ServiceException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user")
 @CrossOrigin
@@ -13,10 +16,17 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private UserServiceInterface userService;
 
-    @GetMapping("/details/{token}")
-    public ResponseEntity<?> getDetailsUser(@PathVariable String token) {
+    @GetMapping("/details")
+    public ResponseEntity<?> getDetailsUser(HttpServletRequest request) {
         try {
-            return ResponseEntity.ok().body(userService.getUserByToken(token));
+            String header = request.getHeader("Authorization");
+
+            if (header == null || !header.startsWith("Bearer ")) {
+                throw new ServiceException("No JWT token found in request headers");
+            }
+
+            String authToken = header.split(" ")[1];
+            return ResponseEntity.ok().body(userService.getUserByToken(authToken));
         } catch (ServiceException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
