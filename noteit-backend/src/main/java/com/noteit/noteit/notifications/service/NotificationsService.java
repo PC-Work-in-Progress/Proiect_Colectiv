7eef150417ea76866133456b9722094953c13e2f
Raul Mag <mag.raul98@gmail.com>
2020-12-25 14:17:48 +0200

    add specs for notifications service

--- noteit-backend/src/main/java/com/noteit/noteit/notifications/service/NotificationsService.java
+++ noteit-backend/src/main/java/com/noteit/noteit/notifications/service/NotificationsService.java
@@ -15,12 +15,24 @@ public class NotificationsService implements NotificationsServiceInterface {
 
     @Autowired
     private NotificationsRepository notificationsRepository;
+
+    /**
+     * function that add a notification that add a notification for a specific user
+     * @param toNotifyUserId the id of user that will get the notification
+     * @param message the message of notification
+     */
     @Override
     public void pushNotification(String toNotifyUserId, String message) {
         Notification notification = new Notification(toNotifyUserId, message, 0);
         notificationsRepository.save(notification);
     }
 
+    /**
+     * function that returns ALL history notifications of a user, each of them having an notification id,
+     * a messsage, and a flag that indicates if a notification was already seen by user until now
+     * @param userId the id of user that gets the notifications
+     * @return list of notifications
+     */
     @Override
     public List<NotificationMessage> getNotifications(String userId) {
         return notificationsRepository.findByUserId(userId).stream()
@@ -28,6 +40,11 @@ public class NotificationsService implements NotificationsServiceInterface {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * @param id the id of some notification that will be marked as viewed
+     * @return Notification onject that represent the notification modified
+     * @throws NotificationAlreadyReadException if the provided id is not a valid id
+     */
     @Override
     public Notification readNotification(String id) throws NotificationAlreadyReadException {
         var notificationOptional = notificationsRepository.findById(id);
